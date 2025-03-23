779353249cfd114fade62b372e55205932bab946
Your Name <you@example.com>
2025-03-23 16:10:55 +0100

    Create api view for classification

--- classifier/views.py
+++ classifier/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from model import classify_image
 
-# Create your views here.
+@api_view(['POST'])
+def classify(request):
+    if 'image' not in request.FILES:
+        return JsonResponse({"error": "No image uploaded"}, status=400)
+    
+    image = request.FILES['image']
+
+    try:
+        predicted_class = classify_image(image)
+    except ValueError as e:
+        return JsonResponse({"error": str(e)}, status=400)
+    
+    return JsonResponse({"class": predicted_class})
