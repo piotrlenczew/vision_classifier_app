ac37df83fda416c761345fb4fd854845171ccf3b
Your Name <you@example.com>
2025-03-23 17:03:30 +0100

    Add basic frontend

--- vision_classifier_app/urls.py
+++ vision_classifier_app/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.views.generic import TemplateView
+from django.urls import path, include
+from classifier.views import classify
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name="index.html"), name='index'),
+    path('classify/', classify, name='classify'),
 ]
